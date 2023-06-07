d537a5dced79bb8e35bb4419a14b9bea09e53850
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-06-07 23:21:42 +0700

    finishing

--- be/server.js
+++ be/server.js
@@ -1,9 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import http from 'http';
 import express from 'express';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 3005;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.SOCKET_ORIGIN,
+        methods: ['GET', 'POST'],
+    }
+});
 
 app.post('/login', function(req, res) {
     res.json({
@@ -17,6 +28,18 @@ app.post('/register', function(req, res) {
     });
 });
 
-app.listen(3033, function() {
-    console.info('server run');
+io.on('connection', function(socket) {
+    console.info('user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });    
+
+    socket.on('sendMessage', function(msg) {
+        io.emit('sendMessage', msg);
+    });
+})
+
+server.listen(PORT, function() {
+    console.info(`server run on http://localhost:${PORT}`);
 });
